c7966a1558445da95b791019de71b94a342a6eec
isatys <isatys.riviere@free.fr>
2024-07-03 18:14:49 +0200

    modifier controller,repository, service et mapper

--- src/main/java/com/BookNest/BookNestCore/service/LivreService.java
+++ src/main/java/com/BookNest/BookNestCore/service/LivreService.java
@@ -3,17 +3,17 @@ package com.BookNest.BookNestCore.service;
 
 import com.BookNest.BookNestCore.dto.LivreDTO;
 
-import java.util.List;
-
 public interface LivreService {
 
-    List<LivreDTO> getAllLivres();
 
     LivreDTO getLivreById(Long id);
+
+
+    /*List<LivreDTO> getAllLivres();
 
     LivreDTO createLivre(LivreDTO livreDTO);
 
     LivreDTO updateLivre(Long id, LivreDTO livreDTO);
 
-    void deleteLivre(Long id);
+    void deleteLivre(Long id);*/
 }
