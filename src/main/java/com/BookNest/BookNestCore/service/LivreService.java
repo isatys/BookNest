7181be979a87e5af16f94cc2c6a89e6433675f8f
isatys <isatys.riviere@free.fr>
2024-07-05 10:54:56 +0200

    modifier controller et service pour voir tous les livres

--- src/main/java/com/BookNest/BookNestCore/service/LivreService.java
+++ src/main/java/com/BookNest/BookNestCore/service/LivreService.java
@@ -3,6 +3,8 @@ package com.BookNest.BookNestCore.service;
 
 import com.BookNest.BookNestCore.dto.LivreDTO;
 
+import java.util.List;
+
 public interface LivreService {
 
 
@@ -10,9 +12,9 @@ public interface LivreService {
 
     LivreDTO createLivre(LivreDTO livreDTO);
 
-    /*List<LivreDTO> getAllLivres();
+    List<LivreDTO> getAllLivres();
 
-
+/*
     LivreDTO updateLivre(Long id, LivreDTO livreDTO);*/
 
     String deleteLivre(Long id);
