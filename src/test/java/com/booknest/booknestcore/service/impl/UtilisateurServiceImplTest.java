8128acae082fca9204fb6af2a2ed27200e7d87cd
isatys <isatys.riviere@free.fr>
2024-09-22 17:20:46 +0200

    erreur de build

--- src/test/java/com/booknest/booknestcore/service/impl/UtilisateurServiceImplTest.java
+++ src/test/java/com/booknest/booknestcore/service/impl/UtilisateurServiceImplTest.java
@@ -5,9 +5,11 @@ import com.booknest.booknestcore.mapper.UtilisateurMapper;
 import com.booknest.booknestcore.model.Utilisateur;
 import com.booknest.booknestcore.repository.UtilisateurRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Arrays;
 import java.util.List;
@@ -17,6 +19,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
  class UtilisateurServiceImplTest {
 
     @Mock
